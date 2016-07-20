2c471f224a296ae495769e0660b24bcbf204889c
jesus <jesus.alberto.vk@gmail.com>
2016-07-19 20:03:03 -0500

    Agregando explicacion de codigos

--- GenerarPalabrasAletorias.rb
+++ GenerarPalabrasAletorias.rb
@@ -1,3 +1,5 @@
+#Genera 100 palabras, puedes cambiar el numero a tu gusto, en el parentesis (0...10) 
+#designamos el tamaño del mismo y los une el resto de la funcion
 100.times do |variable|
 	o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
 	string = (0...10).map { o[rand(o.length)] }.join
--- GenerarPalabrasAletorias2.rb
+++ GenerarPalabrasAletorias2.rb
@@ -1,3 +1,4 @@
+#Usamos la libreria de ruby para general palabras
 require 'securerandom'
 10.times do |variable|
 	random_string = SecureRandom.hex
